7595f8da890ab849ced15239c5fc4d7093f0e812
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 14:51:55 +0700

    commit -2 -main

---
+++ test_3/test_3/Models/EF/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_3.Models.EF
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
