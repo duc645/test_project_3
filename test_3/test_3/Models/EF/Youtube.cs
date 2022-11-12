a3b2cf4c1b0e6bdea2ae37cc39df4a39c7eb183e
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 15:00:29 +0700

    commit -1 -T1

---
+++ test_3/test_3/Models/EF/Youtube.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_3.Models.EF
+{
+    public class Youtube
+    {
+        public string youtube { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
