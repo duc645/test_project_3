3b1c2a20e514f200bd4c7f51a7cbb7dba6f4e365
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 14:58:25 +0700

    commit - 4 -main

---
+++ test_3/test_3/Models/EF/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_3.Models.EF
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
