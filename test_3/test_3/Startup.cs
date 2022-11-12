f21488375068eec71b64862e4cbdabac2c9fdcff
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 14:45:51 +0700

    commit- 1 - main

---
+++ test_3/test_3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_3.Startup))]
+namespace test_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
