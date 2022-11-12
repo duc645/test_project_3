65c756fa75f3cb7e6a3c8b36d7cbd9e9907c2e53
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 14:55:38 +0700

    commit-3-category

---
+++ test_3/test_3/Models/EF/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_3.Models.EF
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Procode { get; set; }
+    }
+}
